bd7672366679f0aa850d4d648a81b1c93454176e
avinashchintareddy <avinash.vs402@gmail.com>
2020-04-28 19:08:00 -0400

    Included in basic navigation and UI

--- app/src/main/java/com/jober/avinashchintareddy/a10khourgoal/MainActivity.kt
+++ app/src/main/java/com/jober/avinashchintareddy/a10khourgoal/MainActivity.kt
@@ -3,9 +3,14 @@ package com.jober.avinashchintareddy.a10khourgoal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
+import com.google.android.material.navigation.NavigationView
 import com.jober.avinashchintareddy.a10khourgoal.models.HourViewModel
 
 
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         hourViewModel = ViewModelProvider(this).get(HourViewModel::class.java)
 
+
         hourViewModel.allSessions.observe(this, Observer { hours ->
             hours?.let {
 
@@ -30,5 +36,21 @@ class MainActivity : AppCompatActivity() {
                 Log.i("MainActivity",hours.get(0).date+"--"+hours.get(0).endTime+"--"+hours.get(0).startTime) }
         })
 
+
+        // get fragment
+        val host: NavHostFragment= supportFragmentManager.
+            findFragmentById(R.id.host_fragment) as NavHostFragment? ?:return
+
+        // add navcontroller
+
+        val navController = host.navController
+
+        //assign
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.host_fragment))
+                ||super.onOptionsItemSelected(item)
     }
 }
